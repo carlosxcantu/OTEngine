d1ffa38265cc51391146188f1fe42088e17d4c44
Carlos Cantu <carlosxcantu@gmail.com>
2026-08-15 07:37:07 -0500

    8/15/26 - Finalized magics.rs

--- src/magics.rs
+++ src/magics.rs
@@ -1,5 +1,104 @@
 use crate::constants::*;
 
+pub struct MagicBitBoards
+{
+    rook_attacks: Vec<u64>,
+    bishop_attacks: Vec<u64>,
+    rook_offsets: [usize; 64],
+    bishop_offsets: [usize; 64],
+    rook_masks: [u64; 64],
+    bishop_masks: [u64; 64],
+}
+
+impl MagicBitBoards
+{
+    pub fn new() -> Self
+    {
+        let mut mb = MagicBitBoards
+        {
+            rook_attacks: vec![0; 102400],
+            bishop_attacks: vec![0; 5248],
+            rook_offsets: [0; 64],
+            bishop_offsets: [0; 64],
+            rook_masks: [0; 64], 
+            bishop_masks: [0; 64],
+        };
+        mb.init();
+        mb
+    }
+
+    pub fn init(&mut self)
+    {
+        let mut current_rook_offset = 0;
+        let mut current_bishop_offset = 0;
+
+        for square in 0..64
+        {
+            // Rook Init
+            self.rook_offsets[square] = current_rook_offset;
+            let rook_mask = calculate_rook_mask(square);
+            self.rook_masks[square] = rook_mask;
+            let rook_bit_count = rook_mask.count_ones();
+            let rook_permutations = generate_permutations(rook_mask);
+
+            for occupancy in rook_permutations 
+            {
+                // The Magic Hashing Formula
+                let magic_index = (occupancy.wrapping_mul(ROOK_MAGICS[square])) >> (64 - rook_bit_count);
+                
+                // The Answer Key
+                let attack_board = calculate_rook_attacks_raycaster(square, occupancy);
+                
+                // Store the answer at the specific offset + the hashed index
+                self.rook_attacks[current_rook_offset + (magic_index as usize)] = attack_board;
+            }
+            current_rook_offset += 1 << rook_bit_count;
+
+            // Bishop Init
+            self.bishop_offsets[square] = current_bishop_offset;
+            let bishop_mask = calculate_bishop_mask(square);
+            self.bishop_masks[square] = bishop_mask;
+            let bishop_bit_count = bishop_mask.count_ones();
+            let bishop_permutations = generate_permutations(bishop_mask);
+
+            for occupancy in bishop_permutations 
+            {
+                let magic_index = (occupancy.wrapping_mul(BISHOP_MAGICS[square])) >> (64 - bishop_bit_count);
+                let attack_board = calculate_bishop_attacks_raycaster(square, occupancy);
+                
+                self.bishop_attacks[current_bishop_offset + (magic_index as usize)] = attack_board;
+            }
+            current_bishop_offset += 1 << bishop_bit_count;
+        }
+    }
+
+    pub fn get_rook_attacks(&self, square: usize, occupancy: u64) -> u64
+    {
+        let mask = self.rook_masks[square];
+        let blocked_occupancy = occupancy & mask;
+
+        // Hash Formula
+        let magic_index = (blocked_occupancy.wrapping_mul(ROOK_MAGICS[square])) >> (64 - mask.count_ones());
+
+        // Offset Lookup
+        let offset = self.rook_offsets[square];
+        self.rook_attacks[offset + (magic_index as usize)]
+    }
+
+    pub fn get_bishop_attacks(&self, square: usize, occupancy: u64) -> u64
+    {
+        let mask = self.bishop_masks[square];
+        let blocked_occupancy = occupancy & mask;
+        
+        // Hash Formula
+        let magic_index = (blocked_occupancy.wrapping_mul(BISHOP_MAGICS[square])) >> (64 - mask.count_ones());
+
+        // Offset Lookup
+        let offset = self.bishop_offsets[square];
+        self.bishop_attacks[offset + (magic_index as usize)]
+    }
+}
+
 pub fn calculate_rook_mask(square: usize) -> u64
 {
     let mut bitboard = 0u64;
